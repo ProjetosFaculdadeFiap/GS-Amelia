f55aa7bbf97bdc30883823e6228c32a516cc0099
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-17 15:16:06 -0300

    finalizando o cabeçalho (preciso parar de mexer nele)

--- src/components/Cabecalho/cabecalho.jsx
+++ src/components/Cabecalho/cabecalho.jsx
@@ -12,14 +12,13 @@ export default function Cabecalho() {
                     height={0} /> 
 
                 <nav>
-                    <Link className="px-20 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> sobre nós </Link>
-                    <Link className="px-10 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> projeto </Link>
-                    <Link className="px-20 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> hapvida </Link>
-                    <Link className="text-[20px]  whitespace-nowrap cabecalho-links" href="/"> home </Link>
+                    <Link className="px-14 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> sobre nós </Link>
+                    <Link className="px-20 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> projeto </Link>
+                    <Link className="px-10 text-[20px]  whitespace-nowrap cabecalho-links" href="/"> hapvida </Link>
+                    <Link className="px-20 text-[20px] whitespace-nowrap cabecalho-links" href="/"> home </Link>
                 </nav>
 
                 <div>
-                    <a className="px-5" href="/users-info/login"> Login </a>
                     <a href="/users-info/cadastro"> 
                         <button className="cabecalho-botao px-3 m-5 transition ease-in-out delay-150 bg-red-300 hover:-translate-y-1 hover:scale-110 hover:bg-red-200 duration-300 ..."> Inscreva-se </button>
                     </a>  

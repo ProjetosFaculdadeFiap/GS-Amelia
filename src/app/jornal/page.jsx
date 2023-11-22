84191599ea02a5c6dd5a86edc427052eaef7c731
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-22 15:54:54 -0300

    amelia :)

--- src/app/jornal/page.jsx
+++ src/app/jornal/page.jsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function Jornal() {
     return (
         <> 
-            <h1> Jornal Amelía </h1>
+            <div className="px-10"> 
+                <Image src="/img/amelia.png"
+                    alt="amelia imagem"
+                    width={200}
+                    height={0} />
+            </div>
+
+            <div> 
+                <p className="introducao-informacao mb-6"> Conhecendo um pouco da Amelía... Venha conhecer como nós produzimos à ideia, à organização, sobre os produtores do projeto, venha conheçer sobre :D</p>
+                <h1> Conhecendo sobre nossa história </h1>
+            </div>
         </>
     )
 }

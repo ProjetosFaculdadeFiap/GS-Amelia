b3ab24ce621f4b399cc9cf7bd4c739a58ebe0fd6
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-24 00:41:35 -0300

    Fix bug in login functionality

--- src/app/formularios/historico/page.jsx
+++ src/app/formularios/historico/page.jsx
@@ -1,3 +1,6 @@
+"use client"; 
+import { useState } from "react";
+
 export default function Historico() {
 
   // Utilização de Token para travar rotas
@@ -9,6 +12,39 @@ export default function Historico() {
   // }, []);
 
     // JAVASCRIPT - HISTÓRICO
+    const [form, setForm] = useState({
+        febre:"", 
+        alcool:"", 
+        alergia:"", 
+        antibiotico:"", 
+        medicamento:"", 
+        doencaPresente:"", 
+        doencaCoracao:"", 
+        fumante:"",
+        diabete:"", 
+        cicatrizacao:"", 
+        gravida:"", 
+        implante:"", 
+    }); 
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const resposta = await fetch("http://localhost:8080/", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(form),
+        });
+
+        if (resposta.ok) {
+            console.log("Prontuário realizado com sucesso :D");
+        } else {
+            console.error('Seu prontuário teve alguma falha, vamos verificar e tente novamente outra hora:', resposta.status, resposta.statusText);
+        }
+    }
 
     return (
         <> 
@@ -35,117 +71,128 @@ export default function Historico() {
                     <div className="conteudo-pai">Histórico Médico:</div>
                     <div className="conteudo-filho mb-10">Adicione aqui todas suas informações médicas!</div>
 
-                    <form> 
+                    <form onSubmit={handleSubmit}> 
                         <div> 
-                            <label className="prontuario"> Teve febre frequentemente? </label>
-                                <input className="m-4" type="checkbox" required />
+                            <label className="prontuario" htmlFor="febre"> Teve febre frequentemente? </label>
+                                <input className="m-4" type="checkbox"  value={form.febre} onChange={handleChange} />
                                 <span>NÃO</span>
 
-                                <input className="m-4" type="checkbox" required />
+                                <input className="m-4" type="checkbox" />
                                 <span>SIM</span>
                         </div>
 
                         <div> 
-                        <label className="prontuario"> Ingestão de álcool? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="alcool"> Ingestão de álcool? </label>
+                            <input className="m-4" type="checkbox"  value={form.alcool} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox" />
                             <span>SIM</span>
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Possui alergias? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="alergias"> Possui alergias? </label>
+                            <input className="m-4" type="checkbox"  value={form.alergias} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox"   value={form.alergias} onChange={handleChange}/>
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quais? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text"  /> 
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Faz uso de antibioticos? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="antibiotico"> Faz uso de antibioticos? </label>
+                            <input className="m-4" type="checkbox"  value={form.antibiotico} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox"  value={form.antibiotico} onChange={handleChange}/>
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quais? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text"  /> 
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Faz uso de medicamentos? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="medicamentos"> Faz uso de medicamentos? </label>
+                            <input className="m-4" type="checkbox"  value={form.medicamentos} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox"  value={form.medicamentos} onChange={handleChange}/>
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quais? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text"  /> 
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Possui alguma doenças presente? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="doencaPresente"> Possui alguma doenças presente? </label>
+                            <input className="m-4" type="checkbox"  value={form.doencaPresente} onChange={handleChange}  />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox" value={form.doencaPresente} onChange={handleChange}/>
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quais? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text"  /> 
                     </div>
                     
                     <div> 
-                        <label className="prontuario"> Possui alguma doenças no coração? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="doencaCoracao"> Possui alguma doenças no coração? </label>
+                            <input className="m-4" type="checkbox"  value={form.doencaCoracao} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox"  />
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quais? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text"  /> 
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Fumante? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="fumante"> Fumante? </label>
+                            <input className="m-4" type="checkbox" value={form.fumante} onChange={handleChange}/>
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox"  value={form.fumante} onChange={handleChange}/>
                             <span>SIM</span>
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Possui diabete? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="diabete"> Possui diabete? </label>
+                            <input className="m-4" type="checkbox" value={form.diabete} onChange={handleChange} />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox" value={form.diabete} onChange={handleChange}  />
                             <span>SIM</span>
                             <label className="prontuario">Se sim, qual tipo? </label>
-                            <input className="prontuario-resposta" type="text" required /> 
+                            <input className="prontuario-resposta" type="text" /> 
                     </div>
 
                     <div> 
-                        <label className="prontuario"> Possui dificuldade de cicatrização </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="cicatrizacao"> Possui dificuldade de cicatrização </label>
+                            <input className="m-4" type="checkbox" value={form.cicatrizacao} onChange={handleChange}  />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox" value={form.cicatrizacao} onChange={handleChange}  />
                             <span>SIM</span>
                     </div>
                     
                     <div> 
-                        <label className="prontuario"> Está grávida? </label>
-                            <input className="m-4" type="checkbox" required />
+                        <label className="prontuario" htmlFor="gravida"> Está grávida? </label>
+                            <input className="m-4" type="checkbox" value={form.gravida} onChange={handleChange}  />
                             <span>NÃO</span>
 
-                            <input className="m-4" type="checkbox" required />
+                            <input className="m-4" type="checkbox" value={form.gravida} onChange={handleChange}  />
                             <span>SIM</span>
                             <label className="prontuario">Se sim, quantos meses? </label>
+                            <input className="prontuario-resposta" type="number"  /> 
+                        </div>
+
+                        <div> 
+                        <label className="prontuario" htmlFor="implante"> Possui implantes? </label>
+                            <input className="m-4" type="checkbox" value={form.implante} onChange={handleChange}  />
+                            <span>NÃO</span>
+
+                            <input className="m-4" type="checkbox" value={form.implante} onChange={handleChange}  />
+                            <span>SIM</span>
+                            <label className="prontuario">Se sim, quais? </label>
                             <input className="prontuario-resposta" type="number" required /> 
                         </div>
 

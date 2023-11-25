ab1fad5ae2dec544b45d73734733dac78502d137
aaaaaaaaana <rm551401@fiap.com.br>
2023-11-24 22:22:29 -0300

    adicionando

--- src/app/formularios/agenda/exame/page.jsx
+++ src/app/formularios/agenda/exame/page.jsx
@@ -1,3 +1,36 @@
+"use client"; 
+import { useEffect, useState } from "react";
+
+export default function Exame() {
+
+  // Utilização de Token para travar rotas
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if (!token) {
+        window.location.href = "/usuarios/login";
+    }
+    }, []);
+
+    
+    const [dia, setDia] = useState('');
+    const [horario, setHorario] = useState('');
+    const [exame, setExame] = useState(''); 
+    const [laboratorio, setLaboratorio] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const resposta = await fetch("http://localhost:8080/mm/exames", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dia, horario, exame, laboratorio),
+        });
+        if (resposta.ok) {
+            console.log("Agendamento realizado com sucesso :D");
+        } else {
+            console.error('Seu agendamento teve alguma falha, vamos verificar e tente novamente outra hora:', resposta.status, resposta.statusText);
+        }
+    }
 
 
 

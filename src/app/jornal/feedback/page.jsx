4afa8ceeb9baaf22b8c8dd3b422d366ea1180fd9
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-24 21:14:24 -0300

    iniciando página feedback - começando com js

--- src/app/jornal/feedback/page.jsx
+++ src/app/jornal/feedback/page.jsx
@@ -1,4 +1,3 @@
-/* 
 "use client"; 
 import { useEffect, useState } from "react";
 
@@ -42,46 +41,7 @@ export default function Feedback() {
 
     return (
         <> 
-            <section className="flex justify-center items-center h-screen">
-            <div className="mx-auto p-10 bg-red-200 rounded-lg shadow-md">
 
-                <form onSubmit={handleSubmit} className="registro-card"> 
-                    <fieldset className="m-0 p-0 border-none"> 
-                    <legend className="text-lg font-bold mb-4">Bem vindo ao Feedback</legend> 
-
-                        <div> 
-                            <label className="mb-2" htmlFor="idNome"> Digite seu nome: </label>
-                            <input className="p-3 my-4 border rounded-md w-full" type="text"  value={nome} onChange={(e) => setNome(e.target.value)} /> 
-                            
-                        </div>
-
-                        <div className="mb-4"> 
-                        <div> 
-                            <label className="mb-2" htmlFor="idDescricao"> O que você achou do projeto? </label>
-                            <input className="p-3 my-4 border rounded-md w-full" type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
-                        
-                        </div>
-                        </div>
-
-                        <div> 
-                            <label className="mb-2" htmlFor="idProposta"> Se pudesse mudar algo ou acresentar o que faria? </label>
-                            <input className="p-3 my-4 border rounded-md w-full" type="text" value={proposta} onChange={(e) => setProposta(e.target.value)} />
-                        </div>
-
-                        <div> 
-                            <label className="mb-2" htmlFor="idIndicacao"> Por onde você nos encontrou? </label>
-                            <input className="p-3 my-4 border rounded-md w-full" type="text" value={indicacao} onChange={(e) => setIndicacao(e.target.value)} />
-                        </div>
-
-                        <div className="flex flex-col items-center"> 
-                            <button className="registro-card-resposta">
-                                Enviar :D 
-                            </button>
-                        </div>
-                    </fieldset>
-                </form>
-            </div>
-            </section>
         </>
     )
-} */ 
+} 

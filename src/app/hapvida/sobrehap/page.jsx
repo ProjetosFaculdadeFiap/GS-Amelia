a6199758d3b18fd92f34f1390be9abea27ca2794
NeugeMa <mariana.n.dourado@gmail.com>
2023-11-24 21:13:00 -0300

    página jornal, está completa :)

--- src/app/hapvida/sobrehap/page.jsx
+++ src/app/hapvida/sobrehap/page.jsx
@@ -21,8 +21,21 @@ export default function SobreHapvida() {
                 <p className="introducao-informacao mb-10"> Conta com rede assistencial própria composta por 85 hospitais, 77 prontos atendimentos e 331 clínicas médicas. Também dispõe aos seus 
                 beneficiários 271 centros de diagnóstico por imagem e coleta laboratorial, dos quais cerca de 100 operações fazem parte do Laboratório NotreLabs, uma rede nacional com capacidade de processar 4,5 milhões de análises clínicas e 500 mil exames de imagens por mês. </p> 
             </div>
-
-
+            <div> 
+                <h2 className="text-2xl px-10"> Conheça nossos valores e objetivos: </h2>
+                    <div className="card-filho"> 
+                        <p className="textinho"> <a class="text-red-400"> Nossa Missão: </a> Proporcionar saúde integrada de qualidade, acessível a geração de brasileiros.</p>
+                        <p className="textinho"> <a class="text-red-400"> Nossa Visão: </a> Ser reconhecida pela qualidade assistencial e geração de valor para todo o ecossistema da saúde.</p>
+                        <p className="textinho"> <a class="text-red-400"> Nossos Valores: </a> Nossos valores se tornam maiores e mais alcansáveis para </p>
+                                <li className="item">Acolhimento</li>
+                                <li className="item">Comprometimento</li>
+                                <li className="item">Ética e combate à corrupção</li>
+                                <li className="item">Inovação e aprendizado</li>
+                                <li className="item">Respeito</li>
+                                <li className="item">Responsabilidade social</li>
+                                <li className="item">Sustentabilidade</li>
+                                <li className="item">Eficiência em custo</li>
+                </div>
             </div>
         </> 
     )   

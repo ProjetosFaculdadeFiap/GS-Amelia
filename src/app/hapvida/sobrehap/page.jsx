8d174fb83fd1fcdab6685a912b0e686ea6da53b6
M1relly <rm98672@fiap.com.br>
2023-11-24 23:13:40 -0300

    inserindo useEffect

--- src/app/hapvida/sobrehap/page.jsx
+++ src/app/hapvida/sobrehap/page.jsx
@@ -2,3 +2,11 @@
 import { useEffect } from "react";
 
 export default function SobreHapvida() {
+
+    // Utilização de Token para travar rotas
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        if (!token) {
+        window.location.href = "/usuarios/login";
+    }
+    }, []);
